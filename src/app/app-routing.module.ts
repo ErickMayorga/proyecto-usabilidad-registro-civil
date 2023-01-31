3d1865898414ec9cc8c423a311c2ae2ec5afe66d
Erick Mayorga <erick.mayorga@epn.edu.ec>
2023-01-31 10:44:17 -0500

    feat: Creación de proyecto y ruta Inicio de Sesión

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioSesionComponent } from './rutas/inicio-sesion/inicio-sesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio-sesion', component: InicioSesionComponent },
+  { path: '', redirectTo: '/inicio-sesion', pathMatch: 'full' },
+  //{ path: '**', component: InicioSesionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioSesionComponent } from './rutas/inicio-sesion/inicio-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioSesionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
