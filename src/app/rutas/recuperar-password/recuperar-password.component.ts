dbb2ab88a4e2bf8e390a0c64313b17605875d0a9
David Aguirre <david.aguirre@epn.edu.ec>
2023-02-26 16:58:53 -0500

    [ADD] [DA] Crear pantalla recuperar contraseña ingreso cédula
    Pantalla de ingreso de cédula para recuperar contraseña

--- src/app/rutas/recuperar-password/recuperar-password.component.ts
+++ src/app/rutas/recuperar-password/recuperar-password.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CampoEntradaInterface } from 'src/app/interfaces/campo-entrada.interface';
+import { TipoCampoEnum } from 'src/app/constants/tipo-campo.enum';
+import { TipoMensajeEnum } from 'src/app/constants/tipo-mensaje.enum';
+import { CampoEntradaService } from 'src/app/servicios/campo-entrada/campo-entrada.service';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-recuperar-password',
   templateUrl: './recuperar-password.component.html',
   styleUrls: ['./recuperar-password.component.scss']
 })
 export class RecuperarPasswordComponent implements OnInit {
+  formGroup! : FormGroup;
+  campos :CampoEntradaInterface[] = [
+    {
+      type: TipoCampoEnum.TEXT,
+      title: 'Cédula',
+      nameField: 'Cédula',
+      helpText: 'Ingrese su número de cédula',
+      screenReaderText: 'Ingresar su número de cédula',
+      placeholder:'Ej.:1713205541',
+      mensajes: [
+        {
+          tipo:TipoMensajeEnum.LONGITUD,
+          textoMensaje: 'La cédula contiene solo 10 caracteres númericos',
+          opciones:{min:10,max:10}
+        },
+        {
+          tipo: TipoMensajeEnum.REQUERIDO,
+          textoMensaje: 'El campo cédula es obligatorio.'
+        },
 
-  constructor() { }
+      ],
+      deshabilitar:false
+    }
+  ]
+  
+
+  constructor(
+    private campoEntradaService:CampoEntradaService,
+    public dialog:MatDialog,
+    private fb:FormBuilder
+  ) { 
+    this.formGroup = this.campoEntradaService.getFormGroup(this.campos)
+  }
 
   ngOnInit(): void {
   }
-
+  validarFormulario(){
+    this.formGroup.addControl('new', this.fb.control(Validators.required))
+  }
 }
