a7825d0a0061933f38aa4986d3be2d20d0279e78
Arie27R <52940197+Arie27R@users.noreply.github.com>
2023-02-26 13:59:08 -0500

    DiseñoTerminado
    Existen bugs pero ya está terminado

--- src/app/rutas/verificacion-datos-personales/lugar.const.ts
+++ src/app/rutas/verificacion-datos-personales/lugar.const.ts
@@ -3,11 +3,68 @@ import {TipoCampoEnum} from "../../constants/tipo-campo.enum";
 import {TipoMensajeEnum} from "../../constants/tipo-mensaje.enum";
 
 export const provincias = [
-  {clave: "1", valor: "Pichincha"},
-  {clave: "2", valor: "Latacunga"},
-  {clave: "3", valor: "Carchi"},
-
+  {clave: "1", valor: "Azuay"},
+  {clave: "2", valor: "Bolívar"},
+  {clave: "3", valor: "Cañar"},
+  {clave: "4", valor: "Carchi"},
+  {clave: "5", valor: "Chimborazo"},
+  {clave: "6", valor: "Cotopaxi"},
+  {clave: "7", valor: "El Oro"},
+  {clave: "8", valor: "Esmeraldas"},
+  {clave: "9", valor: "Galápagos"},
+  {clave: "10", valor: "Guayas"},
+  {clave: "11", valor: "Imbabura"},
+  {clave: "12", valor: "Loja"},
+  {clave: "13", valor: "Los Ríos"},
+  {clave: "14", valor: "Manabí"},
+  {clave: "15", valor: "Morona-Santiago"},
+  {clave: "16", valor: "Napo"},
+  {clave: "17", valor: "Orellana"},
+  {clave: "18", valor: "Pastaza"},
+  {clave: "19", valor: "Pichincha"},
+  {clave: "20", valor: "Santa Elena"},
+  {clave: "21", valor: "Santdo Domingo de los Tsáchilas"},
+  {clave: "22", valor: "Sucumbíos"},
+  {clave: "23", valor: "Tungurahua"},
+  {clave: "24", valor: "Zamora-Chinchipe"}
 ]
+export const cantones = [
+  {clave: "1", valor: "Quito"},
+  {clave: "2", valor: "Cayambe"},
+  {clave: "3", valor: "Mejía"},
+  {clave: "4", valor: "Pedro Moncayo"},
+  {clave: "5", valor: "Pedro Vicente Maldonado"},
+  {clave: "6", valor: "Puerto Quito"},
+  {clave: "7", valor: "Rumiñahui"},
+  {clave: "8", valor: "San Miguel de los Bancos"},
+]
+export const parroquias = [
+  {clave: "1", valor: "Belisario Quevedo"},
+  {clave: "2", valor: "Carcelén"},
+  {clave: "3", valor: "Centro Histórico"},
+  {clave: "4", valor: "Cochapamba"},
+  {clave: "5", valor: "Comité del pueblo"},
+  {clave: "6", valor: "Cotocollao"},
+  {clave: "7", valor: "Chilibulo"},
+  {clave: "8", valor: "Chillogallo"},
+  {clave: "9", valor: "Chimbacalle"},
+  {clave: "10", valor: "El Condado"},
+  {clave: "11", valor: "Guamaní"},
+  {clave: "12", valor: "Iñaquito"},
+  {clave: "13", valor: "Jipijapa"},
+  {clave: "14", valor: "Kennedy"},
+  {clave: "15", valor: "La Argelia"},
+  {clave: "16", valor: "La Concepcion"},
+  {clave: "17", valor: "La Ecuatoriana"},
+  {clave: "18", valor: "La Ferroviaria"},
+  {clave: "19", valor: "La Libertad"},
+  {clave: "20", valor: "La Magdalena"},
+  {clave: "21", valor: "La Mena"},
+  {clave: "22", valor: "Mariscal Sucre"},
+  {clave: "23", valor: "Ponceano"},
+  {clave: "24", valor: "Puengasi"}
+]
+
 
 export const camposLugar: CampoEntradaInterface[] = [
   {
@@ -38,7 +95,7 @@ export const camposLugar: CampoEntradaInterface[] = [
       },
     ],
     deshabilitar: false,
-    opciones: provincias
+    opciones: cantones
   },
   {
     type: TipoCampoEnum.SELECT,
@@ -53,6 +110,7 @@ export const camposLugar: CampoEntradaInterface[] = [
       },
     ],
     deshabilitar: false,
-    opciones: provincias
+    opciones: parroquias
   },
 ]
+
